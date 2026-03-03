82e8affd7ed6555f154dbd73d1240943c2fc5f09
fazila7002 <fazilatunnisa705@gmail.com>
2026-03-03 22:33:00 +0700

    implementasi prosedur dan fugsi

--- persegi2.cpp
+++ persegi2.cpp
@@ -2,3 +2,28 @@
 using namespace std;
 
 int p, l;
+
+void input(){
+    cout << "Masukkan Panjang = ";
+    cin >> p;
+    cout << "Masukkan Lebar = ";
+    cin >> l;
+}
+
+float jumlah(float a, float b, float c){
+    return a + b + c;
+}
+
+int luasPersegiP(int a, int b){
+    return a * b;
+}
+
+void output(){
+    cout << "HAsilnya =" << luasPersegiP(p, l) << endl;
+    cout <<"terimakasih" << endl;
+}
+int main(){
+    input();
+    output();
+    cout << "Hasil penjumlahan=" << jumlah(15.1, 24.9, 67);
+}
